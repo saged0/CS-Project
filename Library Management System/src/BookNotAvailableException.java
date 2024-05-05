4dc8868a17be95d602febf28ec61fff69f08f8a6
Sage Despeignes <131297724+saged0@users.noreply.github.com>
2024-05-05 12:41:22 -0400

    Delete Library Management System directory

--- Library Management System/src/BookNotAvailableException.java
+++
@@ -1,6 +0,0 @@
-// Exception handling
-class BookNotAvailableException extends Exception {
-    public BookNotAvailableException(String message) {
-        super(message);
-    }
-}
--- Library Management System/src/BookNotFoundException.java
+++
@@ -1,5 +0,0 @@
-class BookNotFoundException extends Exception {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
